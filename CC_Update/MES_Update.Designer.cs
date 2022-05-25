9318b5923c94a4af041e4cdd1648e8e53da21d6a
quoctrang0122 <quoctrang0122@gmail.com>
2022-05-25 12:59:20 +0700

    File Upload visual studio

---
+++ CC_Update/MES_Update.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CC_Update
+{
+    partial class MES_Update
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MES_Update));
+            this.pnlLoading = new BevelPanel.AdvancedPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ptxwifi = new System.Windows.Forms.PictureBox();
+            this.timUpdate = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.spinningCircles1 = new AltoControls.SpinningCircles();
+            this.pnlLoading.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptxwifi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLoading
+            // 
+            this.pnlLoading.BackColor = System.Drawing.Color.Transparent;
+            this.pnlLoading.BackgroundGradientMode = BevelPanel.AdvancedPanel.PanelGradientMode.Vertical;
+            this.pnlLoading.Controls.Add(this.label2);
+            this.pnlLoading.Controls.Add(this.pictureBox1);
+            this.pnlLoading.Controls.Add(this.label1);
+            this.pnlLoading.Controls.Add(this.ptxwifi);
+            this.pnlLoading.Controls.Add(this.spinningCircles1);
+            this.pnlLoading.EdgeWidth = 0;
+            this.pnlLoading.EndColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlLoading.FlatBorderColor = System.Drawing.Color.Empty;
+            this.pnlLoading.ForeColor = System.Drawing.Color.Cyan;
+            this.pnlLoading.Location = new System.Drawing.Point(5, 4);
+            this.pnlLoading.Name = "pnlLoading";
+            this.pnlLoading.RectRadius = 10;
+            this.pnlLoading.ShadowColor = System.Drawing.Color.Empty;
+            this.pnlLoading.ShadowShift = 0;
+            this.pnlLoading.ShadowStyle = BevelPanel.AdvancedPanel.ShadowMode.ForwardDiagonal;
+            this.pnlLoading.Size = new System.Drawing.Size(285, 178);
+            this.pnlLoading.StartColor = System.Drawing.Color.WhiteSmoke;
+            this.pnlLoading.Style = BevelPanel.AdvancedPanel.BevelStyle.Flat;
+            this.pnlLoading.TabIndex = 0;
+            this.pnlLoading.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlLoading_Paint);
+            this.pnlLoading.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlLoading_MouseDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(71, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Vui lòng chờ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(118)))), ((int)(((byte)(248)))));
+            this.label1.Location = new System.Drawing.Point(8, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // ptxwifi
+            // 
+            this.ptxwifi.Location = new System.Drawing.Point(254, 4);
+            this.ptxwifi.Name = "ptxwifi";
+            this.ptxwifi.Size = new System.Drawing.Size(25, 25);
+            this.ptxwifi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptxwifi.TabIndex = 1;
+            this.ptxwifi.TabStop = false;
+            // 
+            // timUpdate
+            // 
+            this.timUpdate.Enabled = true;
+            this.timUpdate.Interval = 1000;
+            this.timUpdate.Tick += new System.EventHandler(this.timUpdate_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(106, 70);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // spinningCircles1
+            // 
+            this.spinningCircles1.BackColor = System.Drawing.Color.Transparent;
+            this.spinningCircles1.FullTransparent = true;
+            this.spinningCircles1.Increment = 1F;
+            this.spinningCircles1.Location = new System.Drawing.Point(76, 36);
+            this.spinningCircles1.N = 8;
+            this.spinningCircles1.Name = "spinningCircles1";
+            this.spinningCircles1.Radius = 2.5F;
+            this.spinningCircles1.Size = new System.Drawing.Size(129, 117);
+            this.spinningCircles1.TabIndex = 5;
+            this.spinningCircles1.Text = "spinningCircles1";
+            // 
+            // MES_Update
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(299, 189);
+            this.Controls.Add(this.pnlLoading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MES_Update";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.DimGray;
+            this.pnlLoading.ResumeLayout(false);
+            this.pnlLoading.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptxwifi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BevelPanel.AdvancedPanel pnlLoading;
+        private System.Windows.Forms.Timer timUpdate;
+        private System.Windows.Forms.PictureBox ptxwifi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private AltoControls.SpinningCircles spinningCircles1;
+    }
+}
+
---
+++ CC_Update/MES_Update.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CC_Update
+{
+    public partial class MES_Update : Form
+    {
+        string Server_IP = "192.168.30.103";
+        private string sExcuteFile = "";
+        private string sShowData = "";
+        private string sConnnection = EncryptionQRCode.Encryption.DatabaseConnection("QRCode_Database.bmp");
+        private static bool Finish_Download = false;
+        private static bool First = true;
+        public MES_Update()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlLoading_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Release the mouse capture started by the mouse down.
+                pnlLoading.Capture = false;
+
+                // Create and send a WM_NCLBUTTONDOWN message.
+                const int WM_NCLBUTTONDOWN = 0x00A1;
+                const int HTCAPTION = 2;
+                Message msg =
+                    Message.Create(this.Handle, WM_NCLBUTTONDOWN,
+                        new IntPtr(HTCAPTION), IntPtr.Zero);
+                this.DefWndProc(ref msg);
+            }
+        }
+        public bool Ping_Server(string Server_IP)
+        {
+            bool bOK = false;
+            System.Net.NetworkInformation.Ping p = new System.Net.NetworkInformation.Ping();
+            try
+            {
+                PingReply r = p.Send(Server_IP);
+                if (r.Status == IPStatus.Success)
+                    bOK = true;
+                else
+                    bOK = false;
+            }
+            catch (Exception exMsg)
+            {
+                if (exMsg != null)
+                {
+                    bOK = false;
+                }
+            }
+
+            return bOK;
+        }
+        #region //Error log//
+        public void WriteErrorLog(string sErrorMessage)
+        {
+            FileInfo fiLog = new FileInfo(Application.StartupPath + @"\UpdateLog.txt");
+            StreamWriter swMsg = null;
+            string sLine = null;
+            try
+            {
+                if (fiLog.Exists)
+                {
+                    swMsg = fiLog.AppendText();
+                }
+                else
+                {
+                    swMsg = fiLog.CreateText();
+                    sLine = sLine + DateTime.Now.ToString() + " Create new file" + Environment.NewLine;
+                    swMsg.Write(sLine);
+                }
+                sLine = sLine + DateTime.Now.ToString() + " " + sErrorMessage + Environment.NewLine;
+                swMsg.Write(sLine);
+                swMsg.Flush();
+                swMsg.Close();
+            }
+            catch (Exception exError)
+            {
+                if (exError != null)
+                {
+                    swMsg.Flush();
+                    swMsg.Close();
+                }
+            }
+        }
+        #endregion
+
+        #region //Check file//
+        private bool CheckFile(string FileName)
+        {
+            bool bKeep = false;
+            //if (FileName == "Shoe_Material_Update.exe") bKeep = true;
+            if (FileName == "QRCode_Database.bmp") bKeep = true;
+            //if (FileName == "EncryptionQRCode.dll") bKeep = true;
+            if (FileName == "zxing.dll") bKeep = true;
+            if (FileName == "RFID.ini") bKeep = true;
+            //if (FileName == "Shoe_Material_Update.pdb") bKeep = true;
+            if (!bKeep)
+            {
+                OleDbConnection odcConnect = new OleDbConnection(sConnnection);
+                string sSQL = "SELECT FileName FROM File_Download_New WHERE FileName = ?";
+                OleDbCommand odcCommand = new OleDbCommand(sSQL, odcConnect);
+                odcCommand.Parameters.Add("FileName", OleDbType.VarChar).Value = FileName;
+                odcConnect.Open();
+                OleDbDataReader odrReader = odcCommand.ExecuteReader();
+                try
+                {
+                    while (odrReader.Read())
+                    {
+                        bKeep = true;
+                    }
+                }
+                catch (Exception exMsg)
+                {
+                    if (exMsg != null)
+                    {
+                        WriteErrorLog("CheckFile " + exMsg.ToString());
+                        if (odrReader != null) odrReader.Dispose();
+
+                    }
+                }
+                finally
+                {
+                    if (odrReader != null) odrReader.Dispose();
+                }
+            }
+            return bKeep;
+        }
+        #endregion
+
+        #region //Set excute file//
+        private string ExcuteFile()
+        {
+            string sData = "";
+            OleDbConnection odcConnect = new OleDbConnection(sConnnection);
+            string sSQL = "SELECT FileName FROM File_Download_New WHERE ExcuteFile = 1";
+            OleDbCommand odcCommand = new OleDbCommand(sSQL, odcConnect);
+
+            try
+            {
+                odcConnect.Open();
+                OleDbDataReader odrReader = odcCommand.ExecuteReader();
+                try
+                {
+                    while (odrReader.Read())
+                    {
+                        sData = odrReader["FileName"].ToString().Trim();
+                    }
+                }
+                catch (Exception exMsg)
+                {
+                    if (exMsg != null)
+                    {
+                        WriteErrorLog("CCDownload.DownloadFile " + exMsg.ToString());
+                        if (odrReader != null) odrReader.Dispose();
+
+                    }
+                }
+                finally
+                {
+                    if (odrReader != null) odrReader.Dispose();
+                }
+            }
+            catch (Exception exMsg)
+            {
+                if (exMsg != null)
+                {
+
+                    if (odcCommand != null) odcCommand.Dispose();
+                    if (odcConnect != null) odcConnect.Dispose();
+                }
+            }
+            finally
+            {
+                if (odcCommand != null) odcCommand.Dispose();
+                if (odcConnect != null) odcConnect.Dispose();
+            }
+
+            return sData;
+        }
+        #endregion
+
+        #region //Download file//
+        private void Get_DownloadMax()
+        {
+            First = false;
+            int iCountFile = 0;
+            string sPath = "", sFileName = "";
+            bool bFileExists = false;
+            string[] sFileList = new string[1000];
+
+            foreach (string FileName in System.IO.Directory.GetFileSystemEntries(System.Windows.Forms.Application.StartupPath + @"\\SM_Download", "*.*"))
+            {
+                if (!CheckFile(FileName.Remove(0, System.Windows.Forms.Application.StartupPath.Length + 14))) File.Delete(FileName);
+            }
+
+            OleDbConnection odcConnect = new OleDbConnection(sConnnection);
+            string sSQL = "SELECT FileName, ExcuteFile, Enforce, UpdateTime FROM File_Download_New WHERE UpdateStartDate <= ? AND UpdateDutyDate >= ? ORDER BY UpdateTime";
+            //string sSQL = "SELECT LEN(FileStream) AS FileStream, FileName, ExcuteFile, Enforce, UpdateTime FROM File_Download_New WHERE UpdateStartDate <= ? AND UpdateDutyDate >= ? AND FileName = 'Lacty_HRS.exe'";
+            OleDbCommand odcCommand = new OleDbCommand(sSQL, odcConnect);
+            odcCommand.Parameters.Add("UpdateStartDate", OleDbType.VarChar).Value = DateTime.Now.ToString("yyyyMMdd");
+            odcCommand.Parameters.Add("UpdateDutyDate", OleDbType.VarChar).Value = DateTime.Now.ToString("yyyyMMdd");
+
+            try
+            {
+                odcConnect.Open();
+                OleDbDataReader odrReader = odcCommand.ExecuteReader();
+                try
+                {
+                    while (odrReader.Read())
+                    {
+                        sPath = System.Windows.Forms.Application.StartupPath + @"\\SM_Download\\" + odrReader["FileName"].ToString().Trim();
+                        if (odrReader["ExcuteFile"].ToString().Trim() == "1")
+                            sExcuteFile = odrReader["FileName"].ToString().Trim();
+                        Application.DoEvents();
+                        System.GC.Collect();
+                        System.IO.FileInfo fiImg = new System.IO.FileInfo(sPath);
+                        sFileName = odrReader["FileName"].ToString().Trim();
+                        bFileExists = fiImg.Exists;
+                        if (odrReader["Enforce"].ToString().Trim() == "1")
+                        {
+                            fiImg.Delete();
+                            bFileExists = false;
+                        }
+
+                        if (bFileExists && (fiImg.LastWriteTime < StrToDate(odrReader["UpdateTime"].ToString().Trim())))
+                        {
+                            fiImg.Delete();
+                            bFileExists = false;
+
+                        }
+
+                        if (!bFileExists)
+                        {
+
+                            sFileList[iCountFile] = odrReader["UpdateTime"].ToString().Trim();
+                            iCountFile = iCountFile + 1;
+                        }
+                    }
+                }
+                catch (Exception exMsg)
+                {
+                    if (exMsg != null)
+                    {
+                        WriteErrorLog("CCDownload.DownloadFile " + exMsg.ToString());
+                        if (odrReader != null) odrReader.Dispose();
+                    }
+                }
+                finally
+                {
+                    if (odrReader != null) odrReader.Dispose();
+                }
+            }
+            catch (Exception exMsg)
+            {
+                if (exMsg != null)
+                {
+
+                    if (odcCommand != null) odcCommand.Dispose();
+                    if (odcConnect != null) odcConnect.Dispose();
+                }
+            }
+            finally
+            {
+                if (odcCommand != null) odcCommand.Dispose();
+                if (odcConnect != null) odcConnect.Dispose();
+            }
+
+            for (int i = 0; i < iCountFile; i++)
+            {
+
+                DownloadFile(sFileList[i]);
+            }
+            Finish_Download = true;
+        }
+
+        private void DownloadFile(string UpdateTime)
+        {
+            OleDbConnection odcConnect = new OleDbConnection(sConnnection);
+            string sPath = "";
+            bool bFileExists = false;
+
+            string sSQL = "SELECT FileStream, FileName, ExcuteFile, Enforce, LEN(FileStream) AS LEN_FileStream, UpdateTime  FROM File_Download_New WHERE UpdateTime >= ?";
+            OleDbCommand odcCommand = new OleDbCommand(sSQL, odcConnect);
+            odcCommand.Parameters.Add("UpdateTime", OleDbType.VarChar).Value = UpdateTime;
+            try
+            {
+                odcConnect.Open();
+                OleDbDataReader odrReader = odcCommand.ExecuteReader();
+
+                while (odrReader.Read())
+                {
+                    Application.DoEvents();
+                    if (odrReader["ExcuteFile"].ToString().Trim() == "1")
+                        sExcuteFile = odrReader["FileName"].ToString().Trim();
+                    System.GC.Collect();
+                    sPath = System.Windows.Forms.Application.StartupPath + @"\\SM_Download\\" + odrReader["FileName"].ToString().Trim();
+                    //sPath = @"C:\ERP\" + odrReader["FileName"].ToString().Trim();
+                    System.IO.FileInfo fiImg = new System.IO.FileInfo(sPath);
+                    bFileExists = fiImg.Exists;
+
+                    if (!bFileExists)
+                    {
+                        Application.DoEvents();
+                        //lblFileDownload.Text = odrReader["FileName"].ToString().Trim() + "-->" + "Download";
+                        //lblFileDownload.Refresh();
+                        Byte[] byBlob = null;
+                        byBlob = new Byte[(odrReader.GetBytes(0, 0, null, 0, int.MaxValue))];
+                        odrReader.GetBytes(0, 0, byBlob, 0, byBlob.Length);
+                        FileStream fsImg = null;
+                        fsImg = new FileStream(sPath, FileMode.Create, FileAccess.Write);
+                        fsImg.Write(byBlob, 0, byBlob.Length);
+                        fsImg.Close();
+                       sShowData= odrReader["FileName"].ToString().Trim();
+                        
+                        //prbDownload.Value += Convert.ToInt32(odrReader["LEN_FileStream"].ToString().Trim());
+                    }
+                }
+            }
+            catch (Exception exMsg)
+            {
+                if (exMsg != null)
+                {
+                    WriteErrorLog("CCDownload.DownloadFile " + exMsg.ToString());
+                    if (odcCommand != null) odcCommand.Dispose();
+                    if (odcConnect != null) odcConnect.Dispose();
+                }
+            }
+            finally
+            {
+                if (odcCommand != null) odcCommand.Dispose();
+                if (odcConnect != null) odcConnect.Dispose();
+            }
+
+        }
+
+        #endregion
+
+        #region //Kill process
+        private void Kill_Process()
+        {
+            string sStr = "";
+            OleDbConnection odcConnect = new OleDbConnection(sConnnection);
+            string sSQL = "SELECT FileName  FROM File_Background";
+            OleDbCommand odcCommand = new OleDbCommand(sSQL, odcConnect);
+            try
+            {
+                odcConnect.Open();
+                OleDbDataReader odrReader = odcCommand.ExecuteReader();
+                try
+                {
+                    while (odrReader.Read())
+                    {
+                        Application.DoEvents();
+                        sStr = odrReader["FileName"].ToString().Trim();
+                        Process[] MyProcess = Process.GetProcessesByName(sStr);
+                        if (MyProcess.Length > 0) MyProcess[MyProcess.Length - 1].Kill();
+
+                    }
+                }
+                catch (Exception exMsg)
+                {
+                    if (exMsg != null)
+                    {
+                        WriteErrorLog("CCDownload.DownloadFile " + exMsg.ToString());
+                        if (odrReader != null) odrReader.Dispose();
+
+                    }
+                }
+                finally
+                {
+                    if (odrReader != null) odrReader.Dispose();
+                }
+            }
+            catch (Exception exMsg)
+            {
+                if (exMsg != null)
+                {
+                    WriteErrorLog("CCDownload.DownloadFile " + exMsg.ToString());
+                    if (odcCommand != null) odcCommand.Dispose();
+                    if (odcConnect != null) odcConnect.Dispose();
+                }
+            }
+            finally
+            {
+                if (odcCommand != null) odcCommand.Dispose();
+                if (odcConnect != null) odcConnect.Dispose();
+            }
+        }
+        #endregion
+
+        #region //String change to DateTime//
+        //2011.11.24 Merlin Create
+        //2012.11.21 Merlin 增加一筆2012/11/21 15:02:40:111
+        public DateTime StrToDate(string sDate)
+        {
+            DateTime dtDateTime = DateTime.Now;
+            if (sDate.Length == 8) sDate = sDate.Substring(0, 4) + "-" + sDate.Substring(4, 2) + "-" + sDate.Substring(6, 2) + " 00:00:00";
+            if (sDate.Length == 10) sDate = sDate.Substring(0, 4) + "-" + sDate.Substring(4, 2) + "-" + sDate.Substring(6, 2) + " " + sDate.Substring(8, 2) + ":00:00";
+            if (sDate.Length == 12) sDate = sDate.Substring(0, 4) + "-" + sDate.Substring(4, 2) + "-" + sDate.Substring(6, 2) + " " + sDate.Substring(8, 2) + ":" + sDate.Substring(10, 2) + ":00";
+            if (sDate.Length == 14) sDate = sDate.Substring(0, 4) + "-" + sDate.Substring(4, 2) + "-" + sDate.Substring(6, 2) + " " + sDate.Substring(8, 2) + ":" + sDate.Substring(10, 2) + ":" + sDate.Substring(12, 2);
+            if (sDate.Length == 17) sDate = sDate.Substring(0, 4) + "-" + sDate.Substring(4, 2) + "-" + sDate.Substring(6, 2) + " " + sDate.Substring(8, 2) + ":" + sDate.Substring(10, 2) + ":" + sDate.Substring(12, 2) + "." + sDate.Substring(14, 3);
+            return dtDateTime = Convert.ToDateTime(sDate);
+        }
+        #endregion
+        private void DownloadProgram()
+        {
+            if (First)
+            {
+                Thread tDownload = new Thread(new ThreadStart(Get_DownloadMax));
+                if (Directory.Exists(System.Windows.Forms.Application.StartupPath + @"\\SM_Download\\"))
+                {
+                    //Kill_Process();
+                    //Get_DownloadMax();
+                    tDownload.Start();
+                }
+                else
+                {
+                    System.IO.Directory.CreateDirectory(System.Windows.Forms.Application.StartupPath + @"\\SM_Download\\");
+                    //Kill_Process();
+                    //Get_DownloadMax();
+                    tDownload.Start();
+                }
+            }
+            if (Finish_Download)
+            {
+                sExcuteFile = ExcuteFile();
+                Process.Start(System.Windows.Forms.Application.StartupPath + @"\\SM_Download\\" + sExcuteFile);
+                //if(File.Exists("message.vbs")) Process.Start("message.vbs");
+                //Thread.Sleep(2000);
+                this.Close();
+                Environment.Exit(Environment.ExitCode);//保證一定離開程式
+            }
+        }
+        private void timUpdate_Tick(object sender, EventArgs e)
+        {
+            timUpdate.Enabled = false;
+            DownloadProgram();
+            if (!Finish_Download) timUpdate.Enabled = true;
+            if (Ping_Server(Server_IP))
+            {
+                ptxwifi.Image = Properties.Resources.wifi32;
+            }
+            else
+            {
+                ptxwifi.Image = Properties.Resources.nowifi_32;
+            }
+        }
+
+        private void pnlLoading_Paint(object sender, PaintEventArgs e)
+        {
+            //this.SetStyle(ControlStyles.UserPaint, true);
+            //this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            //this.SetStyle(ControlStyles.DoubleBuffer, true);
+            //Font fFont = new Font("Calibri Bold", 14F);
+            //SolidBrush bBrush = new SolidBrush(Color.FromArgb(1, 118, 248));
+            //e.Graphics.DrawString(sShowData, fFont, bBrush, 20, 20);
+
+            label1.Text = sShowData;
+        }
+
+      
+    }
+}
